7f68d16a414f83250d58d7d80a5585473db26ad4
Chandan Sharma <chandan610sharma@gmail.com>
2021-06-30 05:28:10 +0530

    contest solutions

---
+++ CodeChef_CP/JUNE21B/BitwiseTuples.cpp
@@ -0,0 +1,108 @@
+/* 
+" हर-हर महादेव "
+Author: Chandan Sharma(itsmorris_worm), India
+commit: 4/06/2021
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define IOS ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(0);
+#define vfor0(i,vec) for(int i=0;i<(int)(n.size());i++)
+#define vfor1(j,vec) for(int j=1;j<(int)(n.size());j++)
+#define for0(j,n) for(int j=0;j<(int)(n);j++)
+#define for1(j,n) for(int j=1;j<(int)(n);j++)
+#define all(x) (x).begin(), (x).end() //forward traversal
+#define rall(x) (x).rbegin(), (x).rend()//reverse traversal
+#define tr(vec,i) for(__typeof__((vec)).begin() i=(vec).begin();i!=(vec).end();i++)//iterator
+#define present(vec,x) ((vec).find(x)!=(vec).end())//for set and map(log(n))
+#define cpresent(vec,x) (find(all(vec),x)!=(vec).end())//for all containers
+#define delete(vec,x) vec.erase(std::remove(vec.begin(), vec.end(), x), vec.end())
+#define sz(a) int((a).size())
+#define pb push_back
+#define testcases long long int asdflkj; cin>>asdflkj; while(asdflkj--)
+#define endl "\n"
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define jabtak(t) while(t) 
+#define ll long long 
+#define ld long double 
+#define ull unsigned long long 
+#define pi 3.14159265358979323846264338327950L
+const ll MOD=1e9+7;
+const ll formax = 1e18;
+const ll formin = -1e18;
+
+bool is_integer(int n) {return (((double)sqrt(n)-floor((double)sqrt(n)))==0);}
+int add(int x,int y) {if(y==0) return x; else return add(x^y,(x&y)<<1);}
+int sub(int x,int y) {if(y==0) return x; else return sub(x^y,(~x&y)<<1);}
+//------------------------------------
+ll int power(ll int x,ll int y,ll int MODI)
+{
+    ll int res=1;
+    while(y)
+    {
+        if(y%2)
+            res=(res*x)%MODI;
+            y=y>>1;
+            x=(x*x)%MODI;
+    }
+    return res;
+}
+void chef()
+{
+   ll int N;
+   vector<ll int> X_co,Y_co;
+   for(int i=0;i<N;i++)
+   {
+       ll int input1,input2;
+       cin>>input1>>input2;
+       X_co.push_back(input1);
+       Y_co.push_back(input2);
+   }
+    if(N==1)
+        cout<<0<<endl;
+    else if(N%2==0)
+    {
+        bool flag=false;
+        for(int i=0;i<X_co.size()-1;i+=2)
+        {
+            if((X_co[i]==X_co[i+1]) || (Y_co[i]==Y_co[i+1]))
+                continue;
+            else
+                {
+                    flag=true;
+                    break;
+                }
+        }
+        if(!flag)
+            cout<<0<<endl;
+        else
+            {
+            ll int length=*max_element(X_co.begin(),X_co.end())-1;
+            ll int width=*max_element(Y_co.begin(),Y_co.end())-1;
+            cout<<(length*width)<<endl; 
+            }
+
+    }
+    else
+    {
+        ll int length=*max_element(X_co.begin(),X_co.end())-1;
+        ll int width=*max_element(Y_co.begin(),Y_co.end())-1;
+        cout<<(length*width)<<endl;
+    }
+}
+
+//------------------------------------
+void helper()
+{ testcases { chef();}}
+//------------------------------------
+
+int main()
+{
+IOS;
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+#endif
+helper();
+return 0;
+}
